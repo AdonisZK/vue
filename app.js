0a64ef79e080bb204946b16f3fa52140f7d8c9d2
AdonisZK <adonisvefleyor@gmail.com>
2023-11-01 10:20:43 +0700

    simple calc

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const app = Vue.createApp({
     data() {
         return {
-            items: [
-                { id: 1, text: 'Item 1', style: { backgroundColor: 'white' } },
-                { id: 2, text: 'Item 2', style: { backgroundColor: 'white' } },
-                { id: 3, text: 'Item 3', style: { backgroundColor: 'white' } }
-            ]
+            num1: 0,
+            num2: 0,
+            result: 0
         }
     },
     methods: {
-        changeColor(item) {
-            item.style.backgroundColor = item.style.backgroundColor === 'white' ? 'red' : 'white';
+        add() {
+            this.result = this.num1 + this.num2;
+        },
+        subtract() {
+            this.result = this.num1 - this.num2;
+        },
+        multiply() {
+            this.result = this.num1 * this.num2;
+        },
+        divide() {
+            if (this.num2 !== 0) {
+                this.result = this.num1 / this.num2;
+            } else {
+                this.result = 'Cannot divide by zero';
+            }
         }
     }
 });
 
 app.mount('#app');
-
